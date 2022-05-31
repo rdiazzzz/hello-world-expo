0def44ef1fb3bb527940e63b9b7032bfa113be7f
zenkaa7 <zenkaayo@gmail.com>
2022-05-31 21:46:03 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
 
 export default function App() {
+  function showToast() {
+    ToastAndroid.showWithGravity(
+      "Hello World",
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER
+    );
+  }
+
+  function switchModeToast() {
+    ToastAndroid.showWithGravity(
+      "Switch Light or Dark mode",
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity onPress={showToast}>
+        <Text style={styles.text}>Hello World</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.switchMode} onPress={switchModeToast} />
     </View>
   );
 }
@@ -13,8 +40,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    color: "#4287f5",
+    fontSize: 100,
+  },
+
+  switchMode: {
+    backgroundColor: "#FFCC66",
+    width: 30,
+    height: 30,
+    position: "absolute",
+    top: 30,
+    left: 20,
+    borderRadius: 50,
   },
 });
